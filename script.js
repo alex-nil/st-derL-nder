03fc2191c9810ac8969ceb783308c1f513cd5bea
Alexander Nilsson <nial19qa@student.ju.se>
2019-09-27 10:10:39 +0200

    funktion för att spara länder

--- script.js
+++ script.js
@@ -33,20 +33,39 @@ var norge = document.getElementById("norge");
 
 //Eventlistener för Sverige
 sverige.addEventListener("click", function() {
-    stadH1.innerHTML = "Städer i Sverige";
+    stadLista.innerHTML = "<h1>Städer i Sverige</h1>";
     for (var i = 0; i <städer.length; i++){
         if (städer[i].countryid ===1) {
-            stadLista.innerHTML += "<li><button onclick=\"stadInfo()\" class=\"btn btn-success\" id=\"" + städer[i].id + "\">" + städer[i].stadname + "</button></li>";
+            
+            stadLista.innerHTML += "<li><button onclick=\"stadInfo(this)\" value=\"" + städer[i].id + "\" class=\"btn btn-success\" id=\"" + städer[i].id + "\">" + städer[i].stadname + "</button></li>";
         }
     }
+    
     
 });
 
 
-function clickMe(e) {
-for(var i = 0; i<städer.length; i++) {
-    if (document.getElementsByTagName("button").value === städer[i].stadname) {
-        console.log("yes");
+function stadInfo(objButton) {
+   var knappID = Number(objButton.value);
+    for(var i = 0; i<städer.length; i++) {
+        if (knappID === städer[i].id) {
+            document.getElementById("info").innerHTML = "<h1>" + städer[i].stadname + "</h1>";
+            document.getElementById("info").innerHTML += "<h3>Population:</h3>";
+            document.getElementById("info").innerHTML += "<p>" + städer[i].population + "</p>";
+
+            document.getElementById("besökt").innerHTML = "<button onclick=\"spara(this)\" value=\"" + städer[i].id + "\" class=\"btn btn-success\">Spara Stad</button>";
+        }
     }
+}
+var personer = 0;
+
+function spara(objButton) {
+    var knappID = Number(objButton.value);
+    for(var i = 0; i<städer.length; i++) {
+        if (knappID === städer[i].id) {
+            personer += städer[i].population;
+            localStorage.setItem(JSON.stringify(städer[i].id), JSON.stringify(städer[i].stadname));
+            localStorage.setItem("personer", JSON.stringify(personer));
+        }
     }
 }
